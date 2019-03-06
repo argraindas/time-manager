e8fd8e95f178fe2879e334c92e61accc41d857ce
argraindas <argraindas@gmail.com>
2019-03-06 09:02:13 +0200

    HomeController renamed to PageController

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,4 @@ Route::get('/', function () {
 
 Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'PageController@index')->name('home');
