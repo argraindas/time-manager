98a2039cd523898b1ac8b279b2329740d641592a
argraindas <argraindas@gmail.com>
2019-03-12 22:15:18 +0200

    Paginator component created

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,7 @@
 import './bootstrap';
 
 Vue.component('flash', require('./components/Flash.vue').default);
-// Vue.component('paginator', require('./components/Paginator.vue').default);
-// Vue.component('user-notifications', require('./components/UserNotifications.vue').default);
-
+Vue.component('paginator', require('./components/Paginator.vue').default);
 Vue.component('categories', require('./components/Categories.vue').default);
 
 const app = new Vue({
--- resources/js/components/Categories.vue
+++ resources/js/components/Categories.vue
@@ -4,6 +4,8 @@
             <li v-text="category.name"></li>
         </ul>
 
+        <paginator :dataSet="dataSet" @changed="fetch"></paginator>
+
     </div>
 </template>
 
@@ -19,16 +21,26 @@
         },
 
         created() {
-          this.fetch();
+            this.fetch();
         },
 
         methods: {
-            fetch() {
-                axios.get('/api/categories').then(({data}) => {
+            fetch(page) {
+                axios.get(this.url(page)).then(({data}) => {
                     this.dataSet = data;
                     this.items = data.data;
                 });
             },
+
+            url(page) {
+                if (! page) {
+                    let query = location.search.match(/page=(\d+)/);
+
+                    page = query ? query[1] : 1;
+                }
+
+                return `/api/categories?page=${page}`;
+            }
         }
     };
 </script>
