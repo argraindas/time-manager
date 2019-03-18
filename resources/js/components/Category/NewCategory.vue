69e83f134e7a03b06eabf91673a2e3f1ed0a3309
argraindas <argraindas@gmail.com>
2019-03-18 05:46:03 +0200

    Category style fixe

--- resources/js/components/Category/NewCategory.vue
+++ resources/js/components/Category/NewCategory.vue
@@ -1,19 +1,21 @@
 <template>
 
-    <div class="form-row align-items-center">
+    <div class="form-row align-items-center mb-3">
 
         <div class="col-auto">
-            <div class="input-group input-group-sm mb-3">
+            <div class="input-group input-group-sm">
                 <input type="text" class="form-control input" placeholder="New category" name="name" v-model="form.name" @keydown="form.errors.clear($event.target.name)">
-                <span class="help is-danger" v-if="form.errors.has('name')" v-text="form.errors.get('name')"></span>
+
             </div>
         </div>
         <div class="col-auto">
-            <div class="input-group input-group-sm mb-3">
+            <div class="input-group input-group-sm">
                 <button type="submit" class="btn btn-sm btn-primary" @click="addCategory" :disabled="form.errors.any()">Create</button>
             </div>
         </div>
-
+        <div class="col-auto mt-1">
+            <div class="help is-danger" v-if="form.errors.has('name')" v-text="form.errors.get('name')"></div>
+        </div>
     </div>
 
 </template>
