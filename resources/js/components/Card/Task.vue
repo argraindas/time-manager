a62276f9257922b63153218368f2b282b1d4c341
argraindas <argraindas@gmail.com>
2019-05-16 14:03:15 +0300

    Task can be edited

--- resources/js/components/Card/Task.vue
+++ resources/js/components/Card/Task.vue
@@ -1,18 +1,29 @@
 <template>
-        <div class="task custom-control custom-checkbox d-flex" :class="'task-status-'+status">
-            <div class="task-name">
-                <input type="checkbox" class="custom-control-input" :id="task.id" @click="toggle" :checked="isChecked">
-                <label class="custom-control-label" :for="task.id" v-text="task.name"></label>
-            </div>
-            <div class="task-actions">
-                <a href="#" class="icon-blue" @click.prevent="edit"><i class="material-icons">edit</i></a>
-                <a href="#" class="icon-red" @click.prevent="remove"><i class="material-icons">delete_forever</i></a>
-            </div>
 
-            <transition name="fade">
-                <div v-if="isNew" class="new-item-bg"></div>
-            </transition>
+    <div class="task custom-control custom-checkbox d-flex" :class="'task-status-'+status">
+        <div class="task-name">
+            <input type="checkbox" class="custom-control-input" :id="task.id" @click="toggleStatus" :checked="isChecked">
+            <label v-if="! editing" class="custom-control-label" :for="task.id" v-text="form.name"></label>
+            <div v-if="editing">
+                <form @submit.prevent="update" @keydown="form.errors.clear($event.target.name)">
+                    <div class="input-group input-group-sm">
+                        <input type="text" v-model="form.name" class="form-control">
+                    </div>
+                </form>
+                <div class="help is-danger small-text mt-2" v-if="form.errors.has('name')" v-text="form.errors.get('name')"></div>
+            </div>
         </div>
+        <div class="task-actions">
+            <a href="#" class="icon-blue" @click.prevent="toggleEdit" v-if="! editing"><i class="material-icons">edit</i></a>
+            <a href="#" class="icon-blue" @click.prevent="cancelEdit" v-if="editing"><i class="material-icons">highlight_off</i></a>
+            <a href="#" class="icon-red" @click.prevent="remove"><i class="material-icons">delete_forever</i></a>
+        </div>
+
+        <transition name="fade">
+            <div v-if="isNew" class="new-item-bg"></div>
+        </transition>
+    </div>
+
 </template>
 
 <script>
@@ -21,9 +32,13 @@
 
         data() {
             return {
+                form: new Form({
+                    name: this.task.name,
+                }, false),
                 status: this.task.status,
                 isChecked: this.task.status === 'done',
                 isNew: this.task.isNew ? this.task.isNew: false,
+                editing: false,
             }
         },
 
@@ -34,14 +49,25 @@
         },
 
         created() {
-            setTimeout(() => {
-                this.isNew = false;
-            }, 2000);
+            if (this.isNew) {
+                setTimeout(() => {
+                    this.isNew = false;
+                }, 2000);
+            }
         },
 
         methods: {
-            toggle() {
+            toggleStatus() {
                 this.isChecked ? this.updateStatus('new') : this.updateStatus('done');
+            },
+
+            toggleEdit() {
+                this.editing = ! this.editing;
+            },
+
+            cancelEdit() {
+                this.toggleEdit();
+                this.form.restore();
             },
 
             updateStatus(status) {
@@ -49,20 +75,24 @@
                     .then(({data}) => {
                         this.status = status;
                         flash(data);
-                        this.$emit('updated', data);
                     })
                     .catch(() => flash());
             },
 
-            edit() {
-
+            update() {
+                this.form.patch(this.route('api.tasks.update', {task: this.task.id, card: this.cardId}))
+                    .then((data) => {
+                        this.toggleEdit();
+                        flash(data);
+                    })
+                    .catch(() => flash());
             },
 
             remove() {
                 axios.delete(this.route('api.tasks.destroy', {task: this.task.id, card: this.cardId}))
                     .then(({data}) => {
-                        flash(data);
                         this.$emit('removed', data);
+                        flash(data);
                     })
                     .catch(() => flash());
             }
@@ -106,6 +136,10 @@
             .task-actions{
                 display: block;
             }
+        }
+
+        input[type="text"]{
+            height: 24px;
         }
     }
 
